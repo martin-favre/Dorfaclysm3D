e77bae9434edc698fdd7792ac930cb6eda621a88
martin-favre <martin.favre22@gmail.com>
2020-11-21 19:41:42 +0100

    Logger now prints to unity if warn or error

--- Assets/Scripts/Logging/Logger.cs
+++ Assets/Scripts/Logging/Logger.cs
@@ -15,6 +15,17 @@ namespace Logging
 
         public void Log(string message, LogLevel level)
         {
+            // If it's important, show it in the unity editor
+            if (level == LogLevel.Error)
+            {
+                Debug.LogError(message);
+            }
+            else if (level == LogLevel.Warning)
+            {
+                Debug.LogWarning(message);
+            }
+
+            // Then send it to be written to file
             LogManager.Log(new LogPackage(name, message, level));
         }
         public void Log(string message)

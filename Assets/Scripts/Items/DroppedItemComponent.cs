5eeb59460673130f120ed6b0820bbfc88327b816
Martin Favre <martin.favre22@gmail.com>
2020-12-18 16:34:36 +0100

    Dropped items now fall down in a dreadfully hardcoded fashion However they only drop to the bottom of their block So if the block below is air, it would be nice if it kept falling

--- Assets/Scripts/Items/DroppedItemComponent.cs
+++ Assets/Scripts/Items/DroppedItemComponent.cs
@@ -5,14 +5,70 @@ using Items;
 
 namespace Items
 {
-    public class DroppedItemComponent : MonoBehaviour
+    public class DroppedItemComponent : MonoBehaviour, ISaveableComponent
     {
+        private class SaveData : GenericSaveData<DroppedItemComponent>
+        {
+            public int dropIndex = 0;
+        }
+
         const string prefabName = "Prefabs/ItemContainerObject";
         static GameObject prefabObj;
+
+        static readonly float[] fallPositions = {
+0.25f,
+0.24858767036226256f,
+0.24576301108678766f,
+0.24152602217357533f,
+0.23587670362262553f,
+0.2288150554339383f,
+0.2203410776075136f,
+0.21045477014335146f,
+0.19915613304145188f,
+0.18644516630181485f,
+0.17232186992444037f,
+0.15678624390932847f,
+0.13983828825647907f,
+0.12147800296589226f,
+0.101705388037568f,
+0.08052044347150629f,
+0.05792316926770714f,
+0.033913565426170536f,
+0.008491631946896491f,
+0f,
+0.017371654544170594f,
+0.033330979450603745f,
+0.04787797471929945f,
+0.061012640350257705f,
+0.07273497634347854f,
+0.0830449826989619f,
+0.09194265941670782f,
+0.09942800649671628f,
+0.10550102393898729f,
+0.11016171174352085f,
+0.11341006991031698f,
+0.11524609843937565f,
+0.11566979733069688f,
+0.11468116658428065f,
+0.11228020620012699f,
+0.10846691617823587f,
+0.10324129651860732f,
+0.09660334722124128f,
+0.08855306828613785f,
+0.07909045971329691f,
+0.06821552150271859f,
+0.05592825365440275f,
+0.04222865616834955f,
+0.027116729044558807f,
+0.010592472283030722f,
+0f,
+        };
 
         GridActor actor;
         InventoryComponent inventory;
         BlockVisualizer visualizer;
+
+        SaveData data = new SaveData();
 
         public static DroppedItemComponent InstantiateNew(Vector3Int position)
         {
@@ -47,12 +103,21 @@ namespace Items
                 visualizer = GetComponent<BlockVisualizer>();
                 visualizer.RenderBlock(mainItem.GetTexturePosition());
             }
-            
+
+        }
+
+        void FixedUpdate()
+        {
+            if (data.dropIndex < fallPositions.Length)
+            {
+                transform.position = actor.GetPos() + new Vector3(.25f, -.50f + fallPositions[data.dropIndex], .25f);
+                data.dropIndex++;
+            }
         }
 
         void OnItemAdded()
         {
-            
+
         }
 
         void OnItemRemoved()
@@ -72,6 +137,14 @@ namespace Items
             }
         }
 
+        public IGenericSaveData Save()
+        {
+            return data;
+        }
 
+        public void Load(IGenericSaveData data)
+        {
+            this.data = (SaveData)data;
+        }
     }
 }
